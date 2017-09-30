0e27a0aa3b1b2e003453fd3fb39f56cf0e92fef2
lizz <363664470@qq.com>
2017-09-30 20:49:02 +0800

    chomp chomp

--- src/Shell.jl
+++ src/Shell.jl
@@ -1,6 +1,7 @@
 module Shell
 
 SHELL = is_windows() ? "cmd" : "zsh"
+CHOMP = true
 
 """
     run(cmd::AbstractString; shell=SHELL, capture_output=false)
@@ -8,18 +9,20 @@ SHELL = is_windows() ? "cmd" : "zsh"
 Run your command string in shell.
 
 * To capture output, set `capture_output=true`.
-* To avoid escaping `\$` everytime, you can use raw string, like `raw"echo \$PATH"`
-* You can change the default shell (`zsh` in linux and `cmd` in windows) using `useshell("other_shell")`.
+* To avoid escaping `\$` everytime, you can use raw string,
+  like `raw"echo \$PATH"`
+* You can change the default shell (`zsh` in linux and `cmd` in windows)
+  using `useshell("other_shell")`.
 * In Windows, shell should be `cmd` or `powershell`.
 """
-function run(cmd::AbstractString; shell=SHELL, capture_output=false)
+function run(cmd::AbstractString; shell=SHELL, capture_output=false, chomp=CHOMP)
     if is_windows()
         if shell == "cmd"
             file = "$(tempname()).bat"
             open(f -> println(f, cmd), file, "w")
             if capture_output
                 readstring(`chcp 65001`)
-                return readstring(`$file`)
+                return chomp ? readchomp(`$file`) : readstring(`$file`)
             else
                 return Base.run(`$file`)
             end
@@ -28,7 +31,8 @@ function run(cmd::AbstractString; shell=SHELL, capture_output=false)
             open(f -> println(f, cmd), file, "w")
             if capture_output
                 readstring(`chcp 65001`)
-                return readstring(`powershell -command $file`)
+                return chomp ? readchomp(`powershell -command $file`) :
+                               readstring(`powershell -command $file`)
             else
                 return Base.run(`powershell -command $file`)
             end
@@ -40,16 +44,31 @@ function run(cmd::AbstractString; shell=SHELL, capture_output=false)
         open(f -> println(f, cmd), file, "w")
 
         if capture_output
-            return readstring(`$shell $file`)
+            return chomp ? readchomp(`$shell $file`) : readstring(`$shell $file`)
         else
             return Base.run(`$shell $file`)
         end
     end
 end
 
+"""
+    useshell(shell::AbstractString)
+
+Specify which shell to use (Windows defaults to `cmd`, and `zsh` otherwise).
+"""
 function useshell(shell::AbstractString)
     global SHELL
     SHELL = shell
+end
+
+"""
+    setchomp(chomp::Bool)
+
+Set whether chomp the output (default is true).
+"""
+function setchomp(chomp::Bool)
+    global CHOMP
+    CHOMP = chomp
 end
 
 end # module
