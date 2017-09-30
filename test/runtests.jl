62fe35372691e2e655e1f040e08f3bf509875b9b
lizz <363664470@qq.com>
2017-09-30 21:26:32 +0800

    fix win test

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,7 +14,7 @@ using Base.Test
         @test Shell.run("(Get-Item -Path . -Verbose).FullName", capture_output=true) == pwd()
         Shell.setchomp(false)
         @test endswith(Shell.run("pwd", capture_output=true), "\n")
-        @test !endswith(Shell.run("pwd", capture_output=true, chomp=true), "\n")
+        @test !endswith(Shell.run("(Get-Item -Path . -Verbose).FullName", capture_output=true, chomp=true), "\n")
     else
         Shell.useshell("bash")
         @test Shell.run("ls") == nothing
