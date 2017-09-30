f6778f8e822f2eb3e9f1de94a5468b185720af58
lizz <363664470@qq.com>
2017-09-30 21:22:08 +0800

    add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,26 @@
 using Shell
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@testset "ls" begin
+    if is_windows()
+        @test Shell.run("dir") == nothing
+        @test split(Shell.run("cd", capture_output=true))[end] == pwd()
+        Shell.setchomp(false)
+        @test endswith(Shell.run("pwd", capture_output=true), "\n")
+
+        Shell.useshell("powershell")
+        Shell.setchomp(true)
+        @test Shell.run("ls") == nothing
+        @test Shell.run("(Get-Item -Path . -Verbose).FullName", capture_output=true) == pwd()
+        Shell.setchomp(false)
+        @test endswith(Shell.run("pwd", capture_output=true), "\n")
+        @test !endswith(Shell.run("pwd", capture_output=true, chomp=true), "\n")
+    else
+        Shell.useshell("bash")
+        @test Shell.run("ls") == nothing
+        @test Shell.run("pwd", capture_output=true) == pwd()
+        Shell.setchomp(false)
+        @test endswith(Shell.run("pwd", capture_output=true), "\n")
+        @test !endswith(Shell.run("pwd", capture_output=true, chomp=true), "\n")
+    end
+end
