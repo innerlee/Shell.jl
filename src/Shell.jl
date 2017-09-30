317b4b0fa54a41e5bc2951213fd0344fb342e450
lizz <363664470@qq.com>
2017-09-30 17:20:24 +0800

    try windows

--- src/Shell.jl
+++ src/Shell.jl
@@ -6,7 +6,7 @@ export
     useshell,
     run
 
-SHELL = "zsh"
+SHELL = is_windows() ? "cmd" : "zsh"
 
 """
     run(cmd::AbstractString; shell=SHELL, capture_output=false)
@@ -16,14 +16,26 @@ Run your command string in shell.
 * To capture output, set `capture_output=true`.
 * To avoid escaping `\$` everytime, you can use raw string, like `raw"echo \$PATH"`
 * You can change the default shell (`zsh`) using `useshell("other_shell")`.
+* In Windows, shell should be `cmd`.
 """
 function run(cmd::AbstractString; shell=SHELL, capture_output=false)
-    file = tempname()
+    file = "$(tempname()).bat"
     open(f -> println(f, cmd), file, "w")
+    if is_windows()
+        if shell != "cmd"
+            error("Only support `cmd` in Windows currently...")
+        else
+            if capture_output
+                return readstring(`'$file'`)
+            else
+                return run(`'$file'`)
+            end
+        end
+    end
     if capture_output
         return readstring(`$shell $file`)
     else
-        run(`$shell $file`)
+        return run(`$shell $file`)
     end
 end
 
