4814ddbe8e9c832984b0e3ddd843e6f568bf506c
lizz <363664470@qq.com>
2017-09-30 15:36:53 +0800

    first try

--- src/Shell.jl
+++ src/Shell.jl
@@ -1,5 +1,22 @@
 module Shell
 
-# package code goes here
+import Base.run
+
+export
+    useshell,
+    run
+
+SHELL = "zsh"
+
+function run(cmd::AbstractString; shell=SHELL)
+    file = tempname()
+    open(f -> println(f, cmd), file, "w")
+    run(`$shell $file`)
+end
+
+function useshell(shell::AbstractString)
+    global SHELL
+    SHELL = shell
+end
 
 end # module
