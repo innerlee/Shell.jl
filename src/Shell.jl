a17ddc942a5eccb7a1416e657246d626d1818528
lizz <363664470@qq.com>
2017-09-30 15:56:11 +0800

    add help

--- src/Shell.jl
+++ src/Shell.jl
@@ -8,10 +8,23 @@ export
 
 SHELL = "zsh"
 
-function run(cmd::AbstractString; shell=SHELL)
+"""
+    run(cmd::AbstractString; shell=SHELL, capture_output=false)
+
+Run your command string in shell.
+
+* To capture output, set `capture_output=true`.
+* To avoid escaping `\$` everytime, you can use raw string, like `raw"echo \$PATH"`
+* You can change the default shell (`zsh`) using `useshell("other_shell")`.
+"""
+function run(cmd::AbstractString; shell=SHELL, capture_output=false)
     file = tempname()
     open(f -> println(f, cmd), file, "w")
-    run(`$shell $file`)
+    if capture_output
+        return readstring(`$shell $file`)
+    else
+        run(`$shell $file`)
+    end
 end
 
 function useshell(shell::AbstractString)
