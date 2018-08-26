be7629d275d9f473102c2fc17c3676bca98a50e9
lizz <363664470@qq.com>
2018-08-27 01:58:41 +0800

    ok

--- src/Shell.jl
+++ src/Shell.jl
@@ -2,7 +2,7 @@ module Shell
 
 export @esc_cmd
 
-SHELL   = Sys.iswindows() ? "cmd" : readchomp(pipeline(`getent passwd $(ENV["LOGNAME"])`, `cut -d: -f7`))
+SHELL   = Sys.iswindows() ? "cmd" : readchomp(pipeline(`getent passwd $(get(ENV, "LOGNAME", "root"))`, `cut -d: -f7`))
 CHOMP   = true
 SOURCE  = false
 CAPTURE = false
